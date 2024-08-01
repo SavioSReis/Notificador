1dc34f5abced47a0208c225b3d9339844658b38f
SavioSReis <hjd44.sa@gmail.com>
2024-07-31 22:02:49 -0300

    Função principal

---
+++ PromptNotify/Public/Show-Snackbar.ps1
@@ -0,0 +1,16 @@
+function Show-Snackbar {
+    param (
+        [string]$Titulo,
+        [string]$Mensagem
+    )
+    Process {
+        Add-Type -AssemblyName System.Windows.Forms
+        $Notificacao = New-Object System.Windows.Forms.NotifyIcon
+        $Notificacao.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
+        $Notificacao.BalloonTipText = $Mensagem
+        $Notificacao.BalloonTipTitle = $Titulo
+        $Notificacao.Icon = [System.Drawing.SystemIcons]::Information
+        $Notificacao.Visible = $true
+        $Notificacao.ShowBalloonTip(10000)
+    }
+}
